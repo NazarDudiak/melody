990b0b88aadc603277cedf179d95f5dbd67642a3
Nazar Dudyak <48059672+NazarDudyak@users.noreply.github.com>
2021-08-20 23:43:50 +0300

    Lesson #4

--- js/main.js
+++ js/main.js
@@ -2,31 +2,43 @@
     var currentFloor = 2;
     var counterUp = $('.counter-up');
     var counterDown = $('.counter-down');
-    var floorPath = $('.home-image path')
+    var floorPath = $('.home-image path');
+    var modal = $('.modal');
+    var modalCloseButton = $('.modal-close-button');
+    var viewFlatsButton = $('.view-flats');
+
     floorPath.on('mouseover', function(){
         // console.log('Click');
-        floorPath.removeClass('current-floor')
+        floorPath.removeClass('current-floor');
         currentFloor = $(this).attr('data-floor');
         $('.counter').text(currentFloor);
     });
+
+    floorPath.on('click', toggleModal);
+    modalCloseButton.on('click', toggleModal);
+    viewFlatsButton.on('click', toggleModal);
 
     counterUp.on('click', function () {
         if (currentFloor < 18) {
             currentFloor++;
             usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false});
             $('.counter').text(usCurrentFloor);
-            floorPath.removeClass('current-floor')
-            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor')
-        }
-    })
+            floorPath.removeClass('current-floor');
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+        };
+    });
 
     counterDown.on('click', function () {
         if (currentFloor > 2) {
             currentFloor--;
             usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false});
             $('.counter').text(usCurrentFloor);
-            floorPath.removeClass('current-floor')
-            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor')
-        }
-    })
+            floorPath.removeClass('current-floor');
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+        };
+    });
+
+    function toggleModal(){
+        modal.toggleClass('is-open')
+    }
 });
